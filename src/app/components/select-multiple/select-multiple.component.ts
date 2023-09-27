d7f869d893665657e8cc3c1b40b69a260924963b
Herman Carrasco <hacarrascos@falabella.cl>
2023-09-27 17:57:26 -0300

    :sparkles: Add filter in selectMultiple
    add parameters to show filter and height of options

--- src/app/components/select-multiple/select-multiple.component.ts
+++ src/app/components/select-multiple/select-multiple.component.ts
@@ -15,8 +15,14 @@ export class SelectMultipleComponent implements OnInit {
   constructor() {
     this.list =
       [
-        {name : 'Pendiente etiquetado' , checked : false, value: 'RELABEL_PENDING'},
-        {name :'Listo para despachar' , checked : false, value: 'READY_TO_DISPATCH'},
+        {name : 'J1001 - Jeans Mujer' , checked : false, value: 'RELABEL_PENDING'},
+        {name : 'J1002 - Jeans Hombre' , checked : false, value: 'READY_TO_DISPATCH'},
+        {name : 'T1001 - Consolas' , checked : false, value: 'READY_TO_DISPATCH'},
+        {name : 'T1002 - Juegos consolas' , checked : false, value: 'READY_TO_DISPATCH'},
+        {name : 'O1001 - Otro 1' , checked : false, value: 'READY_TO_DISPATCH'},
+        {name : 'O1002 - Otro 2' , checked : false, value: 'READY_TO_DISPATCH'},
+        {name : 'O1003 - Otro 3' , checked : false, value: 'READY_TO_DISPATCH'},
+
       ];
   }
 
